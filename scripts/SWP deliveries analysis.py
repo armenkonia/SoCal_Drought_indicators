3fcf163fcce64c641741c71d76578e070ff9e5a4
armen.konia <armen.konia@gmail.com>
2024-08-04 23:17:00 -0700

    updated swp deliveries

--- scripts/SWP deliveries analysis.py
+++ scripts/SWP deliveries analysis.py
@@ -7,25 +7,93 @@ Created on Mon Jul 29 09:57:42 2024
 
 import pandas as pd
 excel_path = r"C:/Users/armen/Desktop/Drought Indicators - SoCal/Main/SWP annual total deliveries.xlsx"
-df = pd.read_excel(excel_path, sheet_name='table b-5a', header=[0, 1, 2, 3, 4], index_col=0)
-group_by_column = df.columns.get_level_values(3)
-grouped_df = df.groupby(group_by_column, axis=1).sum()
+table_b5a = pd.read_excel(excel_path, sheet_name='table b-5a', header=[0, 1, 2, 3, 4], index_col=0)
+table_b5a.columns = pd.MultiIndex.from_tuples([
+    (a if 'Unnamed' not in a else '',
+     b if 'Unnamed' not in b else '',
+     c if 'Unnamed' not in c else '',
+     d if 'Unnamed' not in d else '',
+     e if 'Unnamed' not in e else '') for a, b, c, d, e in table_b5a.columns
+])
+
+table_b5a_no_total = pd.read_excel(excel_path, sheet_name='table b-5a modified', header=[0, 1, 2, 3, 4, 5], index_col=0)
+table_b5a_no_total.columns = pd.MultiIndex.from_tuples([
+    (a if 'Unnamed' not in a else '',
+     b if 'Unnamed' not in b else '',
+     c if 'Unnamed' not in c else '',
+     d if 'Unnamed' not in d else '',
+     e if 'Unnamed' not in e else '',
+     f if 'Unnamed' not in f else '') for a, b, c, d, e, f in table_b5a_no_total.columns
+])
+table_b5a_no_total.columns = pd.MultiIndex.from_tuples([(a, b, c, f"{d} {e}".strip(), f) for a, b, c, d, e, f in table_b5a_no_total.columns])
+
 #%%
 # Filter columns where 'met' appears in the column names
-filtered_columns = [col for col in df.columns if 'met' in col[3].lower()]
-filtered_df = df[filtered_columns]
+filtered_columns = [col for col in table_b5a.columns if 'met' in col[3].lower()]
+filtered_df = table_b5a[filtered_columns]
 group_by_column = filtered_df.columns.get_level_values(1)
 met_df = filtered_df.groupby(group_by_column, axis=1).sum()
 met_df.plot()
 met_df['Total'] = met_df.sum(axis=1)
 
-#%%
-import pandas as pd
-excel_path = r"C:/Users/armen/Desktop/Drought Indicators - SoCal/Main/SWP annual total deliveries.xlsx"
 table_b4 = pd.read_excel(excel_path, sheet_name='table b-4', header=[0, 1, 2], index_col=0)
 met_table_a = table_b4
 
 #%%
+#branches
+table_b5a_no_total_modified = table_b5a_no_total.copy()
+table_b5a_no_total_modified.columns = pd.MultiIndex.from_tuples([(b, e) for a, b, c, d, e in table_b5a_no_total_modified.columns])
+column_names = table_b5a_no_total_modified.columns
+grouped_contractors_df = table_b5a_no_total_modified.groupby(column_names,axis=1).sum()
+grouped_contractors_df.columns = pd.MultiIndex.from_tuples(grouped_contractors_df.columns)
 
-group_by_column = df.columns.get_level_values(1)
-grouped_df = df.groupby(group_by_column, axis=1).sum()
+#%%
+#contractors
+table_b5a_no_total_modified = table_b5a_no_total.copy()
+
+contractor_column_names = table_b5a_no_total.columns.get_level_values(2)
+# contractor_column_names = contractor_column_names.unique()
+grouped_contractors_df = table_b5a_no_total.groupby(contractor_column_names, axis=1).sum()
+
+#%%
+#contractors and branch
+table_b5a_no_total_modified = table_b5a_no_total.copy()
+table_b5a_no_total_modified.columns = pd.MultiIndex.from_tuples([(b, d, e) for a, b, c, d, e in table_b5a_no_total_modified.columns])
+column_names = table_b5a_no_total_modified.columns
+grouped_contractors_df = table_b5a_no_total_modified.groupby(column_names,axis=1).sum()
+grouped_contractors_df.columns = pd.MultiIndex.from_tuples(grouped_contractors_df.columns)
+
+#%%
+# branch and HR
+table_b5a_no_total_modified = table_b5a_no_total.copy()
+table_b5a_no_total_modified.columns = pd.MultiIndex.from_tuples([(b, e) for a, b, c, d, e in table_b5a_no_total_modified.columns])
+column_names = table_b5a_no_total_modified.columns
+grouped_contractors_df = table_b5a_no_total_modified.groupby(column_names,axis=1).sum()
+grouped_contractors_df.columns = pd.MultiIndex.from_tuples(grouped_contractors_df.columns)
+#%%
+
+
+# Replace the empty column name with 'FEATHER RIVER AREA'
+new_level_0 = grouped_contractors_df.columns.get_level_values(0).to_series().replace('', 'FEATHER RIVER AREA')
+grouped_contractors_df.columns = pd.MultiIndex.from_arrays([new_level_0, grouped_contractors_df.columns.get_level_values(1)])
+
+# HR to Branch and Regions mapping
+hr_mapping = {
+    'South Coast': ['WEST BRANCH', 'SANTA ANA DIVISION', 'MOJAVE DIVISION'],
+    'Colorado River': ['SANTA ANA DIVISION', 'WEST BRANCH'],
+    'South Lahontan': ['MOJAVE DIVISION', 'WEST BRANCH'],
+    'Tulare Lake': ['COASTAL BRANCH', 'SOUTH SAN JOAQUIN DIVISION', 'TEHACHAPI DIVISION'],
+    'Central Coast': ['COASTAL BRANCH'],
+    'San Joaquin River': ['NORTH SAN JOAQUIN DIVISION'],
+    'Sacramento River': ['FEATHER RIVER AREA'],
+    'San Francisco Bay': ['DELTA DIVISION', 'NORTH SAN JOAQUIN DIVISION']
+}
+
+aggregated_df = pd.DataFrame(index=grouped_contractors_df.index)
+
+# Loop through HR and their respective branches
+for hr, branches in hr_mapping.items():
+    columns_to_sum = pd.IndexSlice[branches, hr]
+    aggregated_df[hr] = grouped_contractors_df.loc[:, columns_to_sum].sum(axis=1)
+    
+#%%
